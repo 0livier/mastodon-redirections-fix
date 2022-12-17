d676f778c3258dd629bd375d216f873e560157d2
Olivier Garcia <olivier@lol.cat>
2022-12-17 19:05:45 +0100

    Changed: More readably JS

--- scripts/content.js
+++ scripts/content.js
@@ -1,24 +1,27 @@
-function replaceText(node) {
-  if (node.nodeType === Node.TEXT_NODE) {
-    node.textContent = node.textContent.replace(/\bElon Musk\b/g, "Space Karen");
+const re = /\bElon Musk\b/gi;
+function replaceElonName(node) {
+  if (node.nodeType === Node.TEXT_NODE && node.textContent.match(re)) {
+    node.textContent = node.textContent.replace(re, "Space Karen");
   } else if (node.nodeType === Node.ELEMENT_NODE) {
     for (const child of node.childNodes) {
-      replaceText(child);
+      replaceElonName(child);
     }
   }
+}
+
+// Avoid modification within the Tweet editor
+function isTweetEditor(node) {
+  return (
+    node.parentNode &&
+    node.parentNode.parentNode &&
+    node.parentNode.parentNode.classList.contains("public-DraftStyleDefault-block")
+  );
 }
 
 const observer = new MutationObserver((mutations) => {
   for (const mutation of mutations) {
     for (const node of mutation.addedNodes) {
-      if (
-        node.parentNode &&
-        node.parentNode.parentNode &&
-        node.parentNode.parentNode.classList.contains("public-DraftStyleDefault-block")
-      )
-        return;
-
-      replaceText(node);
+      if (!isTweetEditor(node)) replaceElonName(node);
     }
   }
 });
