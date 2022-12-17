7dcba201acb93a161809b609fbf3680ac7559e59
Olivier Garcia <olivier@lol.cat>
2022-12-17 15:59:38 +0100

    Fixed: Modifing content was messing with the Draft Editor. Now skipping those elements to ensure the caret stayed as is

--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,6 @@
 function replaceText(node) {
   if (node.nodeType === Node.TEXT_NODE) {
-    node.textContent = node.textContent.replace(
-      /\bElon Musk\b/g,
-      "Space Karen"
-    );
+    node.textContent = node.textContent.replace(/\bElon Musk\b/g, "Space Karen");
   } else if (node.nodeType === Node.ELEMENT_NODE) {
     for (const child of node.childNodes) {
       replaceText(child);
@@ -14,6 +11,13 @@ function replaceText(node) {
 const observer = new MutationObserver((mutations) => {
   for (const mutation of mutations) {
     for (const node of mutation.addedNodes) {
+      if (
+        node.parentNode &&
+        node.parentNode.parentNode &&
+        node.parentNode.parentNode.classList.contains("public-DraftStyleDefault-block")
+      )
+        return;
+
       replaceText(node);
     }
   }
